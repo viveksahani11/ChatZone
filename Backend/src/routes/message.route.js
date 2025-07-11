c81a7b7efb88ddcf6ef1db385750972e3d4d07d5
Vivek Sahani <vivekeditzofficial@gmail.com>
2025-07-11 13:05:43 +0530

    ♻️ Fixed express.Router() space issue in message.route.js

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-const router = express .Router();
+const router = express.Router();
 
 
 router.get("/users",protectRoute, getUsersForSidebar);
